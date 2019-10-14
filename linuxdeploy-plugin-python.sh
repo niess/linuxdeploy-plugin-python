7c1bce2900d20e3acfa8e07f71e213866e0adbf8
anki <anki@code.email>
2019-10-15 02:29:16 +0300

    python entrypoint + xonsh example

--- linuxdeploy-plugin-python.sh
+++ linuxdeploy-plugin-python.sh
@@ -20,6 +20,7 @@ script=$(readlink -f $0)
 exe_name="$(basename ${APPIMAGE:-$script})"
 BASEDIR="${APPDIR:-$(readlink -m $(dirname $script))}"
 
+PYTHON_ENTRYPOINT="${PYTHON_ENTRYPOINT:-}"
 
 # Parse the CLI
 show_usage () {
@@ -153,6 +154,7 @@ do
         cp "${BASEDIR}/share/python-wrapper.sh" "$python"
         sed -i "s|[{][{]PYTHON[}][}]|$python|g" "$python"
         sed -i "s|[{][{]PREFIX[}][}]|$prefix|g" "$python"
+        sed -i "s|[{][{]ENTRYPOINT[}][}]|$PYTHON_ENTRYPOINT|g" "$python"
     fi
 done
 
