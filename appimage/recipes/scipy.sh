24733771105ccd015726acfbac0f952bc0f6400c
anki <anki@code.email>
2019-10-16 22:55:52 +0300

    fix lib path

--- appimage/recipes/scipy.sh
+++ appimage/recipes/scipy.sh
@@ -1,5 +1,5 @@
 export PYTHON_VERSION="3.7.3"
 export PIP_REQUIREMENTS="numpy scipy matplotlib sympy pandas"
 
-export LD_LIBRARY_PATH="AppDir/usr/lib/python3.7/site-packages/numpy/.libs:${LD_LIBRARY_PATH}"
-export LD_LIBRARY_PATH="AppDir/usr/lib/python3.7/site-packages/matplotlib/.libs:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="AppDir/usr/python/lib/python3.7/site-packages/numpy/.libs:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="AppDir/usr/python/lib/python3.7/site-packages/matplotlib/.libs:${LD_LIBRARY_PATH}"
