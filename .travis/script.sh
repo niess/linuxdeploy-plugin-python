dd4ec8c2fee191a2270078ffea079359706ea46a
Valentin Niess <niess@in2p3.fr>
2019-10-18 10:38:08 +0200

    Wrap the build logs for travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,10 +2,24 @@
 
 set -ex
 
-./appimage/build-plugin.sh
-./appimage/build-python.sh python2
-./appimage/build-python.sh python3
-./appimage/build-python.sh scipy
-./appimage/build-python.sh xonsh
+
+wrapped_run() {
+    local script="./appimage/build-$1.sh"
+    local arg="$2"
+    local log
+    if [ -z "$2" ]; then
+        log="plugin.log"
+    else
+        log="${2}.log"
+    fi
+
+    ("${script}" "${arg}" >& "${log}" ; tail -300 "${log}")
+}
+
+wrapped_run plugin
+wrapped_run python python2
+wrapped_run python python3
+wrapped_run python scipy
+wrapped_run python xonsh
 
 python3 -m tests
